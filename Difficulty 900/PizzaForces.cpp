1967b8b934f8343a5195f9c1d91396857a3e56d1
Desmond Wong <dweh29@yahoo.com>
2022-01-30 22:28:32 +0800

    Day 15

---
+++ Difficulty 900/PizzaForces.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Question: https://codeforces.com/problemset/problem/1555/A
+
+int main() 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+
+}
