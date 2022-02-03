31ad31a3aab16d6f05ee3b0ec3c457243e8d75fa
Desmond Wong <dweh29@yahoo.com>
2022-02-03 20:28:04 +0800

    Revert "Day 17"
    This reverts commit 3378133c606d5ba962e8b721c9212b699e7f4461.

--- Template.cpp
+++ Template.cpp
@@ -3,21 +3,10 @@ using namespace std;
 
 // Question: 
 
-void solve()
-{
-
-}
-
 int main() 
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int t;
-    cin >> t;
 
-    while (t--) 
-    {
-        solve();
-    }
 }
