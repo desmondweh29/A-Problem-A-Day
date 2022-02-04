bdd238690e483ff8b2153c2731a5eb1b79958b6c
Desmond Wong <dweh29@yahoo.com>
2022-02-04 18:41:49 +0800

    Day 18

---
+++ Difficulty 900/Bad_Boy.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Question: https://codeforces.com/problemset/problem/1537/B
+
+void solve()
+{
+    long long n, m, i, j;
+    cin >> n >> m >> i >> j;
+
+    long long x1, y1, x2, y2;
+
+    cout << 1 << " " << 1 << " " << n << " " << m << "\n";
+}
+
+int main() 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
