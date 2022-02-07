6fdda3d525d829e76168df329b20f1520ffc0498
Desmond Wong <dweh29@yahoo.com>
2022-02-07 18:50:57 +0800

    Day 21

---
+++ Difficulty 900/Is_it_rated_-2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Question: https://codeforces.com/problemset/problem/1505/A
+
+int main() 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string sen;
+
+    while(cin >> sen)
+    {
+        cout << "NO" << endl;
+    }
+}
